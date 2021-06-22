61c41d606889d25134276c8a6ab2237ba8591bb5
LR-Prime <lalitrajput4050@gmail.com>
2021-06-22 11:34:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/km_miles_converters/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.km_miles_converters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.km_miles_converters.databinding.ActivityMainBinding;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        super.onCreate(savedInstanceState);
+        setContentView(binding.getRoot());
+
+        binding.convertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float Value, ShowValue;
+                Value = Float.valueOf(binding.insertValues.getText().toString());
+                ShowValue = (float) (Value*0.62137);
+                DecimalFormat formatVal = new DecimalFormat("##.#####" );
+                binding.showValues.setText(Value+" Km to Miles = "+formatVal.format(ShowValue));
+            }
+        });
+    }
+    public void clearBTN(View view) {
+        binding.showValues.setText("");
+    }
+}
